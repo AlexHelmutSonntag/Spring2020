388bca5c095abd5dd6d9e5808236c3604f0a176e
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-05-08 17:23:16 +0200

    Create Sampletest1.cpp

---
+++ CppFiles/B1Samples/Sampletest1.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+
+using namespace std;
+
+// Aly Elalwany WGL13Q
+
+/*Input : 6 
+          30 25
+          25 20
+          27 18
+          30 20
+          24 22
+Output : 2
+         5
+         2
+
+
+
+
+*/
+struct temp{
+            int Maxt;
+            int Mint;
+};
+int main()
+{
+  int N;
+  cin >> N;
+  temp Days[N];
+    for (int i=1;i<=N;i++)
+{
+    cin>>Days[i].Maxt >> Days[i].Mint;
+
+}
+int MaxDif = Days[1].Maxt - Days[1].Mint;
+int NCnt=1;
+int OutDays[N];
+OutDays[1]= 1;
+   for (int i=2;i<=N;i++)
+{
+    if((Days[i].Maxt - Days[i].Mint)== MaxDif)
+    {
+        NCnt++;
+        OutDays[NCnt] = i;
+    }
+
+
+   else if((Days[i].Maxt-Days[i].Mint)> MaxDif)
+    {
+
+        OutDays[1] =i;
+        MaxDif = Days[i].Maxt-Days[i].Mint;
+
+    }
+}
+    cout<< NCnt<<endl;
+
+    for(int i=1;i <=NCnt ;i++)
+    {
+        cout<<OutDays[i]<<endl;
+
+    }
+
+
+
+    return 0;
+}
